900d8ede107fc0901ef633c98c34c168c45393af
enda <endacrypto@gmail.com>
2023-11-02 09:14:36 +0000

    working

--- src/IntegrationApp.tsx
+++ src/IntegrationApp.tsx
@@ -1,30 +1,45 @@
 import { FC, useCallback, useEffect, useState } from 'react';
 
+interface Offer {
+  id: string;
+  offerCode: string;
+  name: string;
+}
+interface PageState {
+  selectedOffer?: Offer; 
+  offers?: Offer[];
+}
+
 export const IntegrationApp: FC = () => {
   const [config, setConfig] = useState<Config | null>(null);
   const [projectId, setProjectId] = useState<string | null>(null);
   const [isDisabled, setIsDisabled] = useState(false);
   const [itemName, setItemName] = useState<string | null>(null);
   const [watchedElementValue, setWatchedElementValue] = useState<string | null>(null);
-  const [selectedAssetNames, setSelectedAssetNames] = useState<ReadonlyArray<string>>([]);
-  const [selectedItemNames, setSelectedItemNames] = useState<ReadonlyArray<string>>([]);
+  // const [selectedAssetNames, setSelectedAssetNames] = useState<ReadonlyArray<string>>([]);
+  // const [selectedItemNames, setSelectedItemNames] = useState<ReadonlyArray<string>>([]);
   const [elementValue, setElementValue] = useState<string | null>(null);
 
-  const [offers, setOffers] = useState([]);
+  // const [offers, setOffers] = useState([]);
+  const [state, setState] = useState<PageState>({
+    offers: [],
+    selectedOffer: undefined 
+  });
 
   const fetchProductsData = () => {
-    fetch("https://cors.io/?https://www.bordgaisenergy.ie/api/products/offers")
+    fetch('http://localhost:8080/offers', {mode:'cors'})
       .then(response => {
         return response.json()
       })
       .then(data => {
-        setOffers(data.gasOffers)
+        setState((prevState) => ({
+          ...prevState,
+          offers: data.electricityOffers,
+        }));
       })
   }
 
-  console.log('offers', offers);
   useEffect(() => {
-    console.log('sdsds');
     fetchProductsData();
   }, []);
 
@@ -37,12 +52,18 @@ export const IntegrationApp: FC = () => {
       if (!isConfig(element.config)) {
         throw new Error('Invalid configuration of the custom element. Please check the documentation.');
       }
-
+      console.log('context', context);
       setConfig(element.config);
       setProjectId(context.projectId);
       setIsDisabled(element.disabled);
       setItemName(context.item.name);
       setElementValue(element.value ?? '');
+      if (element.value) {
+        setState((prevState) => ({
+          ...prevState,
+          selectedOffer: JSON.parse(element.value),
+        }));
+      }
       updateWatchedElementValue(element.config.textElementCodename);
     });
   }, [updateWatchedElementValue]);
@@ -67,40 +88,51 @@ export const IntegrationApp: FC = () => {
     CustomElement.observeElementChanges([config.textElementCodename], () => updateWatchedElementValue(config.textElementCodename));
   }, [config, updateWatchedElementValue]);
 
-  const selectAssets = () =>
-    CustomElement.selectAssets({ allowMultiple: true, fileType: 'all' })
-      .then(ids => CustomElement.getAssetDetails(ids?.map(i => i.id) ?? []))
-      .then(assets => setSelectedAssetNames(assets?.map(asset => asset.name) ?? []));
+  // const selectAssets = () =>
+  //   CustomElement.selectAssets({ allowMultiple: true, fileType: 'all' })
+  //     .then(ids => CustomElement.getAssetDetails(ids?.map(i => i.id) ?? []))
+  //     .then(assets => setSelectedAssetNames(assets?.map(asset => asset.name) ?? []));
 
-  const selectItems = () =>
-    CustomElement.selectItems({ allowMultiple: true })
-      .then(ids => CustomElement.getItemDetails(ids?.map(i => i.id) ?? []))
-      .then(items => setSelectedItemNames(items?.map(item => item.name) ?? []));
+  // const selectItems = () =>
+  //   CustomElement.selectItems({ allowMultiple: true })
+  //     .then(ids => CustomElement.getItemDetails(ids?.map(i => i.id) ?? []))
+  //     .then(items => setSelectedItemNames(items?.map(item => item.name) ?? []));
 
-  const updateValue = (newValue: string) => {
-    CustomElement.setValue(newValue);
-    setElementValue(newValue);
+  const selectProductValue = (newValue: string) => {
+    const selectedItem = state.offers && state.offers.find((offer) => offer.id === newValue);
+    setState((prevState) => ({
+      ...prevState,
+      selectedOffer: selectedItem,
+    }));
+  };
+
+  const updateValue = (newValue?: Offer) => {
+    const selectedItem = JSON.stringify(newValue)
+    CustomElement.setValue(selectedItem);
+    setElementValue(selectedItem);
   };
 
   if (!config || !projectId || elementValue === null || watchedElementValue === null || itemName === null) {
     return null;
   }
 
+ 
+  const selectedOffer: Offer = JSON.parse(elementValue);
+  console.log('selectedOffer:', selectedOffer);
+
   return (
     <>
       <h1>
-        This is a great integration with the Kontent.ai app.
+        Product Selector
       </h1>
-      <section>
+      {/* <section>
         projectId: {projectId}; item name: {itemName}
       </section>
       <section>
         configuration: {JSON.stringify(config)}
-      </section>
-      <section>
-        <input value={elementValue} onChange={e => updateValue(e.target.value)} disabled={isDisabled} />
-      </section>
-      <section>
+      </section> */}
+     
+      {/* <section>
         This is the watched element: {watchedElementValue}
       </section>
       <section>
@@ -110,15 +142,68 @@ export const IntegrationApp: FC = () => {
       <section>
         These are your selected item names: {selectedItemNames.join(', ')}
         <button onClick={selectItems}>Select different items</button>
-      </section>
-      <section>
+      </section> */}
       {/* {offers.length > 0 && (
         <ul>
           {offers.map(offer=> (
-            <li key={offer.id}>{offer.name} - {offer.offerCode}</li>
+            <li key={offer.id} onClick={e => updateValue(offer.name)}>{offer.name} - {offer.offerCode}</li>
           ))}
         </ul>
       )} */}
+
+      <section>
+
+        <select onChange={e => selectProductValue(e.target.value)} defaultValue={''} value={selectedOffer.id}>
+          <option value="" disabled>Choose a Product ...</option>
+          {state.offers && state.offers.map(offer=> (
+            <option key={offer.id} value={offer.id}>{offer.name} - {offer.offerCode}</option>
+          ))}
+        </select>
+        {state && state.selectedOffer && (
+          <>
+          <br /><br />
+          Id
+          <br />
+          <input value={state.selectedOffer.id} name="id" onChange={(e)=> {
+            const offer = Object.assign({}, state.selectedOffer, {id: e.target.value});
+            setState((prevState) => ({
+              ...prevState,
+              selectedOffer: offer,
+            }));
+          }} />
+          <br /><br />
+          name
+          <br />
+          <input value={state.selectedOffer.name} name="productName" onChange={(e)=> {
+            const offer = Object.assign({}, state.selectedOffer, {name: e.target.value});
+            setState((prevState) => ({
+              ...prevState,
+              selectedOffer: offer,
+            }));
+          }} />
+          <br /><br />
+          offerCode
+          <br />
+          <input value={state.selectedOffer.offerCode} name="offerCode" onChange={(e)=> {
+            const offer = Object.assign({}, state.selectedOffer, {offerCode: e.target.value});
+            setState((prevState) => ({
+              ...prevState,
+              selectedOffer: offer,
+            }));
+          }} />
+          <br /><br />
+          <button onClick={() => updateValue(state.selectedOffer)}>Save</button>
+          </>
+        )}
+      </section>
+      {/* <textarea cols={100} rows={20}>
+        {elementValue}
+      </textarea> */}
+      <section>
+      <br/><br/>
+        debug:<br/><br/>
+        <textarea rows={10} cols={100} defaultValue={elementValue}></textarea>
+        {/* <input defaultValue={elementValue} disabled={isDisabled} /> */}
       </section>
 
     </>
